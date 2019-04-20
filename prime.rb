62c60a1ccd7b97a19dea9bb38ee2d2c8f86b41e4
Fabian Fabro <fabrofabian@gmail.com>
2019-04-20 02:48:10 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,13 @@
 # Add  code here!5
 def prime?(integer)
-    prime = true
-
     if integer <= 1
-      return prime = false
+      return false
     else
       for divisors in (2..integer) do
           if integer%divisors == 0
-            return prime = false
+            return false
           end
       end
-      return prime
+      return true
     end
 end
