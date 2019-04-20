0921f573976eed1970aabc299bae113ba8f7e9d9
Fabian Fabro <fabrofabian@gmail.com>
2019-04-20 02:26:09 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+# Add  code here!5
+def prime?(integer)
+    if integer < 1
+      return false
+    else
+      for divisors in (2..integer) do
+          if integer%divisors == 0
+            return false
+          else
+            return true
+          end
+      end
+    end
+end
