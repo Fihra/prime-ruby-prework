8d15d62c843c10db5833db5a3c7134f481300792
Fabian Fabro <fabrofabian@gmail.com>
2019-04-20 03:09:35 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,7 @@
 # Add  code here!5
 def prime?(integer)
-  (2..integer-1).none? {|divisor| integer%divisor == 0}
+  if integer < 1
+    return false
+  else
+    (2..integer-1).none? {|divisor| integer%divisor == 0}
 end
