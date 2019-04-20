8488877885b0572c6b1a7c75ddb1d6e59e2eedf4
Fabian Fabro <fabrofabian@gmail.com>
2019-04-20 02:37:45 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -6,8 +6,10 @@ def prime?(integer)
       for divisors in (2..integer) do
           if integer%divisors == 0
             return false
+          else
+            return true
           end
-      return true
+      
       end
     end
 end
